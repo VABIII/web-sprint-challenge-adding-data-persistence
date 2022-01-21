5f115485032d97f629c0c49eb3e35a0c30b5d64e
VABIII <tbengtson82@gmail.com>
2022-01-20 18:11:11 -0600

    Wrote scripts, built out index.js and server.js

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// start your server here
+require('dotenv').config();
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+    console.log(`**** Server running on port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
